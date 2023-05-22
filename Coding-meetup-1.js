4591e813e386db559bf5c1b9922db9f2723d1e25
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-21 21:21:08 -0700

    Create Coding-meetup-1.js

---
+++ Coding-meetup-1.js
@@ -0,0 +1,13 @@
+/*
+You will be given an array of objects representing data about developers who have 
+signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+Notes:
+  The format of the strings will always be Europe and JavaScript.
+  All data will always be valid and uniform as in the example above.
+*/
+
+
+
